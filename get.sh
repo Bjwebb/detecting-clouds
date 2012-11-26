04662b85acf19bd3c396a362bbab6bb8554c0e3a
Ben Webb <bjwebb67@googlemail.com>
2012-11-26 00:07:35 +0000

    Add functionality to sum total image intensity

--- get.sh
+++ get.sh
@@ -1,1 +1,1 @@
-wget -m -c --no-parent "http://www.mrao.cam.ac.uk/~dfb/allsky/2011/06/"
+wget -m -c --no-parent "http://www.mrao.cam.ac.uk/~dfb/allsky/2011/"
--- process.py
+++ process.py
@@ -3,8 +3,7 @@ import pyfits
 import numpy
 from functools import partial
 import scipy.misc.pilutil as smp
-import os, shutil, subprocess
-orig_path = 'www.mrao.cam.ac.uk/~dfb/allsky'
+import sys, os, shutil, subprocess
 
 remove_saturated = numpy.vectorize(lambda x: 0 if x < 0 or x > 65000 else x)
 def flatten_max(max):
@@ -23,13 +22,16 @@ def output(path, data, filter=False, image=False, extract=True):
         img = smp.toimage(flatten_max(5000)(data))
         img.save(os.path.join('out',path[:-5]+'.png'))
 
-def process_night(orig_path, night_path, filter=True, image=True):
+def process_night(orig_path, night_path, filter=True, image=True, do_output=True, do_diff=True, extraf=None):
     prevdata = None
     if filter:
-        for a in 'filtered', 'out':
-            for b in '', 'extracted':
-                for c in '', 'diff':
-                    os.makedirs(os.path.join(a,b,c,night_path))
+        if do_output:
+            for a in 'filtered', 'out':
+                for b in '', 'extracted':
+                    for c in '', 'diff':
+                        if c == 'diff' and not do_diff:
+                            continue
+                        os.makedirs(os.path.join(a,b,c,night_path))
 
     for name in sorted(os.listdir(os.path.join(orig_path,night_path))):
         if not name.endswith('.fits'):
@@ -39,27 +41,73 @@ def process_night(orig_path, night_path, filter=True, image=True):
         data = pyfits.getdata(fname, 0)
 
 
-        if prevdata is not None:
-            diff = remove_saturated( data - prevdata )
-            output(os.path.join('diff',night_path,name),
-                   diff, filter, image)
-        prevdata = data
+        if do_diff:
+            if prevdata is not None:
+                if filter:
+                    diff = remove_saturated( data - prevdata )
+                else:
+                    diff = data - prevdata
+                if do_output:
+                    output(os.path.join('diff',night_path,name),
+                           diff, filter, image)
+            prevdata = data
 
-        data = remove_saturated(data)
-        output(os.path.join(night_path,name),
-               data, filter, image)
-
-for directory in ['filtered', 'out']:
-    try:
-        shutil.rmtree(directory)
-    except OSError:
-        pass
+        if filter:
+            data = remove_saturated(data)
+        if extraf:
+            extraf(name, data)
+        if do_output:
+            output(os.path.join(night_path,name),
+                   data, filter, image)
 
 def sorted_dirs(path):
     return sorted([ d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d)) ])
 
-for year in sorted_dirs(orig_path):
-    for month in sorted_dirs(os.path.join(orig_path,year)):
-        for night in sorted_dirs(os.path.join(orig_path,year,month)):
-            process_night(orig_path, os.path.join(year,month,night))
 
+def generate_out(orig_path):
+    for directory in ['filtered', 'out']:
+        try:
+            shutil.rmtree(directory)
+        except OSError:
+            pass
+
+    for year in sorted_dirs(orig_path):
+        for month in sorted_dirs(os.path.join(orig_path,year)):
+            for night in sorted_dirs(os.path.join(orig_path,year,month)):
+                process_night(orig_path,
+                              os.path.join(year,month,night),
+                              do_diff=False)
+
+def generate_sum(orig_path):
+    for year in sorted_dirs(orig_path):
+        try: int(year)
+        except ValueError: continue
+        day = 0
+        for month in sorted_dirs(os.path.join(orig_path,year)):
+            for night in sorted_dirs(os.path.join(orig_path,year,month)):
+                def days(d):
+                    return day + float(d[8:10])/24 + float(d[10:12])/(24*60)
+                def do_sum(name, data):
+                    print days(name[:-5]), sum(sum(data))
+                process_night(orig_path,
+                              os.path.join(year,month,night),
+                              image=False,
+                              do_output=False,
+                              do_diff=False,
+                              filter=False,
+                              extraf=do_sum)
+                day += 1
+
+if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        print "Supply an argument."
+    elif sys.argv[1] == 'out':
+        generate_out('www.mrao.cam.ac.uk/~dfb/allsky')
+    elif sys.argv[1] == 'sum':
+        generate_sum('www.mrao.cam.ac.uk/~dfb/allsky')
+
+"""
+
+orig_path = 'filtered'
+
+"""

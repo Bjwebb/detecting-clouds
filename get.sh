df9b111084323b993954d301bd2cf6885fe7508c
Ben Webb <bjwebb67@googlemail.com>
2013-01-18 16:30:28 +0000

    Actually save cat file etc

--- get.sh
+++ get.sh
@@ -1,1 +1,1 @@
-wget -m -c --no-parent "http://www.mrao.cam.ac.uk/~dfb/allsky/2011/"
+wget -m --no-parent "http://www.mrao.cam.ac.uk/~dfb/allsky/"
--- process.py
+++ process.py
@@ -9,29 +9,39 @@ remove_saturated = numpy.vectorize(lambda x: 0 if x < 0 or x > 65000 else x)
 def flatten_max(max):
     return numpy.vectorize(lambda x: x if x < max else max)
 
-def output(path, data, filter=False, image=False, extract=True):
+def output(path, data, filter=False, image=False, extract=True, outdir='out'):
     if filter:
-        filtered_file = os.path.join('filtered',path)
-        pyfits.writeto(filtered_file, data) # clobber=True ?
+        filtered_file = os.path.join(outdir, 'fits_filtered',path)
+        pyfits.writeto(filtered_file, data, clobber=True) 
         if extract:
-            extfile = os.path.join('filtered','extracted',path) 
+            extfile = os.path.join(outdir, 'fits_filtered','extracted',path) 
             subprocess.call(['sextractor','-c','test.sex',filtered_file])
             shutil.move('check.fits', extfile)
-            output(os.path.join('extracted',path), pyfits.getdata(extfile, 0), image=True)
+            shutil.move('test.cat',
+                os.path.join(outdir, 'cat', path[:-5]+'.cat')) 
+            output(os.path.join('extracted',path), pyfits.getdata(extfile, 0), image=True, outdir=outdir)
     if image:
         img = smp.toimage(flatten_max(5000)(data))
-        img.save(os.path.join('out',path[:-5]+'.png'))
+        img.save(os.path.join(outdir,'png',path[:-5]+'.png'))
 
-def process_night(orig_path, night_path, filter=True, image=True, do_output=True, do_diff=True, extraf=None):
+def process_night(orig_path, night_path, filter=True, image=True, do_output=True, do_diff=True, extraf=None, outdir='out'):
     prevdata = None
     if filter:
         if do_output:
-            for a in 'filtered', 'out':
+            for a in 'fits_filtered', 'png':
                 for b in '', 'extracted':
                     for c in '', 'diff':
                         if c == 'diff' and not do_diff:
                             continue
-                        os.makedirs(os.path.join(a,b,c,night_path))
+                        try:
+                            os.makedirs(os.path.join(outdir,a,b,c,night_path))
+                        except OSError: pass
+            for c in '', 'diff':
+                if c == 'diff' and not do_diff:
+                    continue
+                try:
+                    os.makedirs(os.path.join(outdir,'cat',c,night_path))
+                except OSError: pass
 
     for name in sorted(os.listdir(os.path.join(orig_path,night_path))):
         if not name.endswith('.fits'):
@@ -54,7 +64,7 @@ def process_night(orig_path, night_path, filter=True, image=True, do_output=True
                     diff = data - prevdata
                 if do_output:
                     output(os.path.join('diff',night_path,name),
-                           diff, filter, image)
+                           diff, filter, image, outdir)
             prevdata = data
 
         if filter:
@@ -63,25 +73,26 @@ def process_night(orig_path, night_path, filter=True, image=True, do_output=True
             extraf(name, data)
         if do_output:
             output(os.path.join(night_path,name),
-                   data, filter, image)
+                   data, filter, image, outdir)
 
 def sorted_dirs(path):
     return sorted([ d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d)) ])
 
 
-def generate_out(orig_path):
-    for directory in ['filtered', 'out']:
-        try:
-            shutil.rmtree(directory)
-        except OSError:
-            pass
-
-    for year in sorted_dirs(orig_path):
-        for month in sorted_dirs(os.path.join(orig_path,year)):
-            for night in sorted_dirs(os.path.join(orig_path,year,month)):
-                process_night(orig_path,
-                              os.path.join(year,month,night),
-                              do_diff=False)
+def generate_out(orig_path, extra_path='', outdir='out'):
+    if extra_path:
+        process_night(orig_path,
+                      night_path,
+                      do_diff=False,
+                      outdir=outdir)
+    else:
+        for year in sorted_dirs(orig_path):
+            for month in sorted_dirs(os.path.join(orig_path,year)):
+                for night in sorted_dirs(os.path.join(orig_path,year,month)):
+                    process_night(orig_path,
+                                  os.path.join(year,month,night),
+                                  do_diff=False,
+                                  outdir=outdir)
 
 def generate_sum(orig_path):
     for year in sorted_dirs(orig_path):
@@ -104,12 +115,14 @@ def generate_sum(orig_path):
                 day += 1
 
 if __name__ == '__main__':
+    orig_path = 'www.mrao.cam.ac.uk/~dfb/allsky'
+    night_path = ''
+    if len(sys.argv) > 2:
+        night_path = sys.argv[2]
+
     if len(sys.argv) <= 1:
-        print "Supply an argument."
+        print "Usage python process.py (out|sum) [night_path]"
     elif sys.argv[1] == 'out':
-        generate_out('www.mrao.cam.ac.uk/~dfb/allsky')
+        generate_out(orig_path, night_path)
     elif sys.argv[1] == 'sum':
-        if len(sys.argv) > 2:
-            generate_sum(sys.argv[2])
-        else:
-            generate_sum('www.mrao.cam.ac.uk/~dfb/allsky')
+        generate_sum(orig_path)

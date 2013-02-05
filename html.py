f80aea0d3a77fe783ad358c303b4c72dff07eb24
Ben Webb <bjwebb67@googlemail.com>
2013-02-05 17:16:50 +0000

    Fix html

--- html.py
+++ html.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os, sys, json
 from urllib import quote
-from process import subdir
+from process import get_subdir
 outdir = sys.argv[1]
 
 f_in = open('head.html', 'r') 
@@ -10,11 +10,16 @@ f_out = open(os.path.join(outdir, 'ani.html'), 'w')
 f_out.writelines(f_in)
 f_in.close()
 
-meta = json.load(open(os.path.join('out','meta',subdir(outdir), 'sum.json'), 'r'))
+try:
+    meta = json.load(open(os.path.join('out','meta',get_subdir(outdir), 'sum.json'), 'r'))
 
-for i, f in enumerate(sorted(os.listdir(outdir))):
-    if f.endswith('.png') and not f.endswith('total.png'):
-        f_out.write('<a href="{1}"><img id="pic"{0}" src="{1}" /></a> {2}<br/>\n'.format(i,quote(f), str(float(meta[f[:-4]])/(10**9))+' *10^9' ))
+    for i, f in enumerate(sorted(os.listdir(outdir))):
+        if f.endswith('.png') and not f.endswith('total.png'):
+            f_out.write('<div class="image"><a href="{1}"><img id="pic"{0}" src="{1}" /></a> {2}<br/></div>\n'.format(i,quote(f), str(float(meta[f[:-4]])/(10**9))+' *10^9' ))
+except IOError: 
+    for i, f in enumerate(sorted(os.listdir(outdir))):
+        if f.endswith('.png') and not f.endswith('total.png'):
+            f_out.write('<div class="image"><a href="{1}"><img id="pic"{0}" src="{1}" /></a></div>\n'.format(i,quote(f)))
 
 f_out.write('</head></body>')
 f_out.close()
